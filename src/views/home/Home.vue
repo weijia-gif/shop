0ec8148b575eb3992dbb3292277013367ac3c4f5
weijia <547064536@qq.com>
2020-12-10 09:46:06 +0800

    shop

---
+++ src/views/home/Home.vue
@@ -0,0 +1,24 @@
+<!--  -->
+<template>
+  <div>首页</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
+<style scoped>
+</style>
